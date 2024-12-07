ad1a99167b3172a387d80b7b09ff384f7beeac5d
dimitri <dyaryarashvili@gmail.com>
2024-12-07 23:27:26 +0400

    feat: added redis and vector

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,3 +1,5 @@
+import { ragChat } from '@/lib/rag-chat';
+import { redis } from '@/lib/redis';
 import React from 'react'
 
 interface PageProps {
@@ -6,10 +8,31 @@ interface PageProps {
     }
 }
 
-const page = ( { params } : PageProps) => {
-    console.log(params);
+function reconstructUrl({url}: {url:string[]}) {
+    const decodedComponents = url.map((component) => decodeURIComponent(component))
+
+    return decodedComponents.join('/')
+}
+
+const page = async ( { params } : PageProps) => {
+    const reconstructedUrl = reconstructUrl({url: params.url as string[]})
+
+    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrl)
+
+    const sessionId = "mock-session"
+
+    if (!isAlreadyIndexed) {
+        await ragChat.context.add({
+            type: "html",
+            source: reconstructedUrl,
+            config: {chunkOverlap: 50, chunkSize: 200},
+        })
+
+        await redis.sadd("indexed-urls", reconstructedUrl)
+    }
+
   return (
-    <div>page</div>
+    <ChatWrapper />
   )
 }
 
